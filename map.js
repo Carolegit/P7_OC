532717fa155411a23eb69544ccf8b985456adc8a
Carolegit <carole.ravohi@gmail.com>
2018-09-11 22:53:26 +0200

    Ajout coordonnées longitude/latitude de la ville de Lyon[F

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
-var map = new ol.Map({
-        target: 'map',
+var map = new ol.Map({//création de l'objet map
+        target: 'map', //liaison de l'objet map au div
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
           })
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
+          center: ol.proj.fromLonLat([4.85, 45.75]),//lon/lat
+          zoom: 15
         })
       });
