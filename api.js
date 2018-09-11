b52787cb9a662d2d9b54fb69c76410295255d42f
Carolegit <carole.ravohi@gmail.com>
2018-09-11 23:14:20 +0200

    Ajout de l'url et la clé API de JCDecaux

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-//API JC DEceaux
+//API JCDECAUX
 
-/*ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={api_key}", function (reponse) {
+ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={abef7229ea73522a353862f2ecac92efef075430}", function (reponse) {
 
-});*/
+});
